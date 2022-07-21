3a75bb72b360db8ee19d3ce6fe133e6cfd34f5e2
gabisegger <gabi.segger@gmail.com>
2022-07-21 11:12:50 -0300

    feat: DELETE route

--- index.js
+++ index.js
@@ -17,11 +17,16 @@ app.use(express.json()) //middleware
 //app.route('/').post( (req,res) => res.send(req.body))
 
 //PUT - alterar
-let author = "Gabriela"
+//let author = "Gabriela"
 
-app.route('/').get( (req,res) => res.send(author) )
+//app.route('/').get( (req,res) => res.send(author) )
 
-app.route('/').put( (req,res) => {
-    author = req.body.author
-    res.send(author)
+// app.route('/').put( (req,res) => {
+//     author = req.body.author
+//     res.send(author)
+// })
+
+//DELETE - deletar informação
+app.route('/:identificador').delete( (req,res) => {
+    res.send(req.params.identificador)
 })
