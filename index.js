21b27819a6c7f1f1358ceffed3fcb56540918301
gabisegger <gabi.segger@gmail.com>
2022-07-21 10:52:13 -0300

    feat: POST route

--- index.js
+++ index.js
@@ -6,5 +6,12 @@ const app = express();
 app.listen('3000'); 
 
 //rotas
+
+//GET
 app.route('/').get( (req,res) => res.send("hello"))
 app.route('/sobre').get( (req,res) => res.send("hello sobre"))
+
+//POST
+//middleware
+app.use(express.json())
+app.route('/').post( (req,res) => res.send(req.body))
