3db9deade799a0e0d40451d6c02db5f4f5a54425
gabisegger <gabi.segger@gmail.com>
2022-07-22 08:25:11 -0300

    feat: consuming github API using axios

--- index.js
+++ index.js
@@ -1,32 +1,15 @@
-//código server
 const express = require('express');
+const axios = require('axios');
 
 const app = express();
 
 app.listen('3000'); 
 
-//rotas
+app.route('/').get( (req, res) => {
+    
+    axios.get('https://api.github.com/users/gabisegger')
+    .then(result => res.send(`<img src="${result.data.avatar_url}"/>`))
+    .catch(error => console.error(error))
 
-app.use(express.json()) //middleware
+} )
 
-//GET - receber
-//app.route('/').get( (req,res) => res.send("hello"))
-//app.route('/sobre').get( (req,res) => res.send("hello sobre"))
-
-//POST - enviar
-//app.route('/').post( (req,res) => res.send(req.body))
-
-//PUT - alterar
-//let author = "Gabriela"
-
-//app.route('/').get( (req,res) => res.send(author) )
-
-// app.route('/').put( (req,res) => {
-//     author = req.body.author
-//     res.send(author)
-// })
-
-//DELETE - deletar informação
-app.route('/:identificador').delete( (req,res) => {
-    res.send(req.params.identificador)
-})
